437c5d10c503b624371c650a68dac14eb517acdd
dtarun2202 <dtarun2202@gmail.com>
2026-09-14 11:49:49 +0530

    our team

--- frontend/src/pages/CSR/CSR.tsx
+++ frontend/src/pages/CSR/CSR.tsx
@@ -184,14 +184,12 @@ const CSR = () => {
             <CsrTeacherTrainingSection />
 
             <div className="media-recognition-banner csr-partners-banner">
-                <div className="media-header">
-
+                <div className="partners-header">
                     <h3 className="media-title">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#e44249" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="leaf-icon left-leaf"><path d="M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z"/><path d="M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12"/></svg>
                         IGNIS IN MEDIA
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#e44249" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="leaf-icon right-leaf"><path d="M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z"/><path d="M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12"/></svg>
                     </h3>
-                    <div className="media-placeholder"></div>
                 </div>
                 
                 <div className="media-carousel-wrapper">
